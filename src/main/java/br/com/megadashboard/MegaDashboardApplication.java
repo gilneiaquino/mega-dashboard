0bacb06acd5456297254d040f27eb1306c384f12
Gilnei aquino <gilnei.aquino@gmail.com>
2025-12-04 10:00:08 -0300

    Projeto base gerado banco ainda

---
+++ src/main/java/br/com/megadashboard/MegaDashboardApplication.java
@@ -0,0 +1,13 @@
+package br.com.megadashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/megadashboard/MegaDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.megadashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
